4a9f3de7101b67d299f73dcf2081e814752c9442
Leonardo Cavalari <leonardo.cavalari@anexus.com.br>
2020-09-28 09:40:41 -0300

    alteracoes

---
+++ src/core/Router.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AnexusPHP\Core;
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+class Router extends SimpleRouter
+{
+    public static function start(): void
+    {
+        //Arquivo de Métodos Globais
+        require_once 'Helpers.php';
+
+        Migration::init();
+
+        $arrUrl = explode('/', trim(url()->getPath(), '/'));
+        $app = isset($arrUrl[0]) && trim($arrUrl[0]) != '' ? $arrUrl[0] : 'app';
+        $app = ucfirst($app);
+        if (!is_dir(PATH_ROUTES . $app)) {
+            $app = 'App';
+        }
+
+        $scanDir = scandir(PATH_ROUTES . $app);
+        unset($scanDir[0]);
+        unset($scanDir[1]);
+        $scanDir = array_values($scanDir);
+
+
+        $j = count($scanDir);
+        for ($i = 0; $i < $j; $i++) {
+            require_once PATH_ROUTES . $app . DS . $scanDir[$i];
+        }
+
+        parent::start();
+    }
+}
