88ff6720238164912d31ab629cc56def7fc67cbe
Leonardo Cavalari <leonardo.cavalari@anexus.com.br>
2020-10-21 11:52:11 -0300

    Alteracoes

---
+++ migration/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.configuration (id,value,description) VALUES
+	 ('MIGRATION_STARTED','true','Migration iniciada'),
+	 ('MIGRATION_VERSION','0.0','Versão da Migration'),
+	 ('TRANSLATION_VERSION','0','Versão das traduções');
