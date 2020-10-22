c76396e6d130839f64960113a2812908c96678ef
Vinicius Vidal <guilherme.vidal@anexus.com.br>
2020-10-22 15:04:46 -0300

    Comando create-route

--- src/Setup/Anx.php
+++ src/Setup/Anx.php
@@ -22,7 +22,8 @@ class Anx
             'create-biz' => '\AnexusPHP\Setup\Setup\Biz',
             'create-biz-module' => '\AnexusPHP\Setup\Setup\BizModule',
             'create-biz-entity' => '\AnexusPHP\Setup\Setup\BizEntity',
-            'create-panel' => '\AnexusPHP\Setup\Setup\Panel'
+            'create-panel' => '\AnexusPHP\Setup\Setup\Panel',
+            'create-route' => '\AnexusPHP\Setup\Setup\Route'
         ];
 
         // verificando se a chave pedida existe
---
+++ src/Setup/Setup/Route.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AnexusPHP\Setup\Setup;
+
+use AnexusPHP\Interfaces\Anx\AnxInterface;
+use AnexusPHP\Setup\Anx;
+use Exception;
+
+class Route extends Anx implements AnxInterface
+{
+    public function __construct($param, $option)
+    {
+        $this->run($param, $option);
+    }
+
+    public function run(array $params = [], array $option = []):void
+    {
+        try {
+            if (!is_writable(PATH_ROOT)) {
+                throw new Exception("");
+            }
+
+            // if (!file_exists(PATH_LOGS . 'start_execution')) {
+            //     throw new Exception('Please start the application', 1);
+            // }
+
+
+            if (!isset($params['-a']) || trim($params['-a'] == '')) {
+                throw new Exception('Error: param -a [app-name] is required', 1);
+            }
+
+            $app = ucwords($params['-a']);
+
+            if (!is_dir(PATH_ROOT . 'src/' . $app)) {
+                throw new Exception("The '{$app}' application doesn't exists", 1);
+            }
+
+            if (!isset($params['-m']) || trim($params['-m'] == '')) {
+                throw new Exception('Error: param -m [module-name] is required', 1);
+            }
+
+            $module = ucwords($params['-m']);
+
+            if (!is_dir(PATH_ROOT . 'src/' . $app . DS . 'Modules' . DS . $module)) {
+                throw new Exception("The '{$module}' module doesn't exists", 1);
+            }
+
+            $path = strtolower( '/' . ($app . '/' . $module));
+
+            if(in_array('--crud', $option)) {
+                $routeType = 'Crud';
+            } else {
+                $routeType = 'Index';
+            }
+
+            $route = $this->getTemplate('Route' . DS . $routeType.'Route', [
+                '{{app}}' => $app,
+                '{{module}}' => $module,
+                '{{prefix}}' => $path,
+                '{{route}}' => ($app . $module)
+            ]);
+
+            //exit(chr(10) . 'ate agr deu certo' . chr(10));
+
+            $files = [
+                PATH_ROUTES . $app . DS . $module . 'Routes.php' => $route
+            ];
+
+            echo "\033[0;37m";
+
+            foreach ($files as $key => $value) {
+                $this->file_force_contents($key, $value);
+            }
+
+        } catch (Exception $e) {
+            exit(chr(10) . $e->getMessage() . chr(10));
+        }
+    }
+
+    public static function help()
+    {
+        echo "    ___    _   ___  __" . chr(10);
+        echo "   /   |  / | / / |/ /" . chr(10);
+        echo "  / /| | /  |/ /|   / " . chr(10);
+        echo " / ___ |/ /|  //   |  " . chr(10);
+        echo "/_/  |_/_/ |_//_/|_|  " . chr(10);
+        echo "                      " . chr(10);
+
+        echo "\033[1;33m" . "Usage:" . "\033[1;37m" . chr(10);
+        echo "\tphp anx create-route [params]" . chr(10) . chr(10);
+
+        echo "\033[1;33m" . "Params:" . "\033[1;37m" . chr(10);
+        echo "\t-a [app-name]" . chr(10) ;
+        echo "\t-m [module-name]" . chr(10);
+        echo "\t--crud - Opitional: generate file with crud routes defined" . chr(10);
+        echo "\t--help - See this helper" . chr(10);
+
+
+        exit(chr(10));
+    }
+}
