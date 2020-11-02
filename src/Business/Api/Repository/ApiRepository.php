5f606d41dc26207ce62f59194a5961db55b2b33d
Leonardo Cavalari <leonardo.cavalari@anexus.com.br>
2020-11-02 14:01:15 -0300

    Alterações

--- src/Business/Api/Repository/ApiRepository.php
+++ src/Business/Api/Repository/ApiRepository.php
@@ -4,6 +4,8 @@ namespace AnexusPHP\Business\Api\Repository;
 
 use AnexusPHP\Business\Api\Entity\ApiEntity;
 use AnexusPHP\Core\Database;
+use AnexusPHP\Core\Libraries\Pagination\Pagination;
+use PDO;
 
 class ApiRepository
 {
@@ -22,5 +24,38 @@ class ApiRepository
         }
 
         return $reg;
+    }
+
+    /**
+     * Retorna os registro do banco com paginacao
+     * 
+     * @param string $url
+     * @param array $filters
+     * @param int $currentPg
+     * @param string $varPg
+     * @param integer $perPg
+     * @return Pagination[]
+     */
+    public static function allWithPagination($url, $filters = array(), $currentPg, $varPg = 'pg', $perPg = 12)
+    {
+        $db = Database::getInstance();
+
+        $bind = array();
+        $where = "";
+
+        if (isset($filters['code']) && trim($filters['code']) != '') {
+            $where .= " code like :code ";
+            $bind['code'] = $filters['code'];
+        }
+
+        $total = $db->query('select count(1) as total from ' . ApiEntity::TABLE . ' a where ' . $where, $bind)->fetch();
+
+        $pagination = new Pagination($total['total'], $perPg, $varPg, $currentPg, $url);
+
+        $regs = $db->query('select a.* from ' . ApiEntity::TABLE . ' a where ' . $where . ' order by a.id desc limit ' . $perPg . ' OFFSET ' . $pagination->getOffset(), $bind)->fetchAll(PDO::FETCH_CLASS, ApiEntity::class);
+
+        $pagination->setRows($regs);
+
+        return $pagination;
     }
 }
