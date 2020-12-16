335f764ea4efeda52658bd59e5818d11a1fb3c6a
Vinicius Vidal <guilherme.vidal@anexus.com.br>
2020-12-16 17:31:30 -0300

    Function dd() para debugar variaveis

--- src/Core/Helpers.php
+++ src/Core/Helpers.php
@@ -224,3 +224,7 @@ function GUID()
 
     return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 }
+
+function dd($value) {
+    var_dump($value);die();
+}
