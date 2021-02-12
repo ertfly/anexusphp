521abbb3f8ed2f8bb67ea92f57d9012fd998f414
Leonardo Cavalari <leonardo.cavalari@anexus.com.br>
2021-02-12 20:05:01 -0300

    Correções

--- src/Business/Configuration/Repository/ConfigurationRepository.php
+++ src/Business/Configuration/Repository/ConfigurationRepository.php
@@ -4,6 +4,7 @@ namespace AnexusPHP\Business\Configuration\Repository;
 
 use AnexusPHP\Business\Configuration\Entity\ConfigurationEntity;
 use AnexusPHP\Core\Database;
+use PDO;
 
 class ConfigurationRepository
 {
@@ -36,5 +37,19 @@ class ConfigurationRepository
         $reg = $db->query('select value from ' . ConfigurationEntity::TABLE . ' where id = :id limit 1', ['id' => $id])->fetchColumn();
 
         return $reg;
+    }
+
+    /**
+     * Retorna um array com todas as configurações que tiverem a string fornecida
+     *
+     * @param string $match
+     * @return ConfigurationEntity[]
+     */
+    public static function byMatch(string $match)
+    {
+        $db = Database::getInstance();
+        $regs = $db->query('select * from ' . ConfigurationEntity::TABLE . " where lower(id) like concat('%', lower(:match), '%') order by id asc", ['match' => $match])->fetchAll(PDO::FETCH_CLASS, ConfigurationEntity::class);
+
+        return $regs;
     }
 }
