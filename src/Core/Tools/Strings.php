753715052ad10a63a7faf34b2d7b00a98a279170
Eric Teixeira <contato@ericteixeira.com.br>
2021-03-15 12:07:15 -0300

    novo metodo para converter arquivo base64 para arquivo

--- src/Core/Tools/Strings.php
+++ src/Core/Tools/Strings.php
@@ -208,4 +208,30 @@ class Strings
             'body' => $body
         ];
     }
+
+    public static function base64ToFile($fileEncoded, $pathDest, array $checkMimes, $errorMsg)
+    {
+        $mimeEncoded = substr($fileEncoded, 0, strpos($fileEncoded, ';'));
+        $mimeEncoded = str_replace('data:', '', $mimeEncoded);
+
+        if (!in_array($mimeEncoded, $checkMimes)) {
+            throw new \Exception($errorMsg);
+        }
+
+        $decoded = substr($fileEncoded, strpos($fileEncoded, ',') + 1);
+        $decoded = str_replace(' ', '+', $decoded);
+
+        $extensions = array(
+            'image/png' => 'png',
+            'image/jpeg' => 'jpg',
+            'image/jpg' => 'jpg',
+        );
+        $filename = Strings::token() . '.' . $extensions[$mimeEncoded];
+        @file_put_contents($pathDest . $filename, base64_decode($decoded));
+        if (!is_file($pathDest . $filename)) {
+            throw new \Exception('Ocorreu um erro ao realizar o upload do arquivo, favor tentar novamente.');
+        }
+
+        return $filename;
+    }
 }
