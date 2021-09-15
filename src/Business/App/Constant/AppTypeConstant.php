57199c14ed93c1d772c24c454c6fbc5d2689c9ac
Eric Teixeira <contato@ericteixeira.com.br>
2021-09-15 17:33:20 -0300

    ajustes

--- src/Business/App/Constant/AppTypeConstant.php
+++ src/Business/App/Constant/AppTypeConstant.php
@@ -2,9 +2,36 @@
 
 namespace AnexusPHP\Business\App\Constant;
 
+use Exception;
+
 class AppTypeConstant
 {
     const BROWSER = 'N';
     const ANDROID = 'A';
     const IOS = 'I';
+    public static $options = [
+        self::BROWSER => 'Navegador de internet',
+        self::ANDROID => 'Android',
+        self::IOS => 'IOS',
+    ];
+    public static function getOptions()
+    {
+        return self::$options;
+    }
+    public static function getOption($key)
+    {
+        if (!isset(self::$options[$key])) {
+            throw new Exception('Opção inválida!');
+        }
+
+        return self::$options[$key];
+    }
+    public static function has($key)
+    {
+        if (!isset(self::$options[$key])) {
+            return false;
+        }
+
+        return true;
+    }
 }
