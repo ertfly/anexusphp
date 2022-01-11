1e855410aa5e44eb501925cc7b10580c4b920a7e
Eric Teixeira <ertfly@gmail.com>
2022-01-10 22:05:07 -0300

    ajustes

--- src/Core/Tools/Number.php
+++ src/Core/Tools/Number.php
@@ -5,26 +5,14 @@ namespace AnexusPHP\Core\Tools;
 class Number
 {
 
-    public static function toDecimal($number, $dec = 2)
+    public static function toDecimal($number, $dec = 2, $usa = false)
     {
-        $before = 0;
-        $after = 0;
-        if (preg_match("/[^\d\,\.]/", $number)) {
-            return 0;
+        if ($usa) {
+            $number = str_replace(',', '', $number);
+        } else {
+            $number = str_replace(',', '.', str_replace('.', '', $number));
         }
-        if (preg_match("/^[\d]{1,}$/", $number)) {
-            $before = $number;
-        }
-        if (preg_match("/([0-9\,\.]{0,})\.([0-9]{1,})$/", $number, $match)) {
-            $before = str_replace('.', '', str_replace(',', '', $match[1]));
-            $after = $match[2];
-        }
-
-        if (preg_match("/([0-9\,\.]{0,})\,([0-9]{1,})$/", $number, $match)) {
-            $before = str_replace('.', '', str_replace(',', '', $match[1]));
-            $after = $match[2];
-        }
-        return number_format($before . '.' . $after, $dec, '.', '');
+        return number_format($number, $dec, '.', '');
     }
 
     public static function key($size = 6)
