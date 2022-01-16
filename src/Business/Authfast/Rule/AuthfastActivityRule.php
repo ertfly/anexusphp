16af591fe0b44b563b1e0686816c66e0db1f9312
Eric Teixeira <ertfly@gmail.com>
2022-01-16 17:07:38 -0300

    ajustes

--- src/Business/Authfast/Rule/AuthfastActivityRule.php
+++ src/Business/Authfast/Rule/AuthfastActivityRule.php
@@ -9,6 +9,17 @@ use Exception;
 
 class AuthfastActivityRule
 {
+    public static function install()
+    {
+        $db = Database::getInstance();
+        $db->authfast_activity->createIndex([
+            'authfast_id' => 1,
+        ], ['name' => 'authfast_activity_idx_authfast_id']);
+        $db->authfast_activity->createIndex([
+            'created_at' => 1,
+        ], ['name' => 'authfast_activity_idx_created_at']);
+        Database::closeInstance();
+    }
     public static function insert(AuthfastActivityEntity &$record)
     {
         $db = Database::getInstance();
