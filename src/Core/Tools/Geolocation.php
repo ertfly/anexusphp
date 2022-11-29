63ee612c43d6ac0c4b5785a121d362a2791d8bf2
Eric Teixeira <ertfly@gmail.com>
2022-11-29 01:58:34 -0300

    fix

--- src/Core/Tools/Geolocation.php
+++ src/Core/Tools/Geolocation.php
@@ -38,28 +38,18 @@ class Geolocation
         return ($kilometers > 0 ? $kilometers . 'km e ' : '') . $meters . ' metros';
     }
 
-    public static function isInPolygon()
+    public static function isInPolygon($lat, $lng, array $verticesY, array $verticesX)
     {
-        $vertices_x = array(37.628134, 37.629867, 37.62324, 37.622424);    // x-coordinates of the vertices of the polygon
-        $vertices_y = array(-77.458334, -77.449021, -77.445416, -77.457819); // y-coordinates of the vertices of the polygon
-        $points_polygon = count($vertices_x) - 1;  // number vertices - zero-based array
-        $longitude_x = $_GET["longitude"];  // x-coordinate of the point to test
-        $latitude_y = $_GET["latitude"];    // y-coordinate of the point to test
+        $pointsPolygon = count($verticesX) - 1;  // number vertices - zero-based array
+        $latitudeY = $lat;    // y-coordinate of the point to test
+        $longitudeX = $lng;  // x-coordinate of the point to test
 
-        if (is_in_polygon($points_polygon, $vertices_x, $vertices_y, $longitude_x, $latitude_y)) {
-            echo "Is in polygon!";
-        } else echo "Is not in polygon";
-
-
-        function is_in_polygon($points_polygon, $vertices_x, $vertices_y, $longitude_x, $latitude_y)
-        {
-            $i = $j = $c = 0;
-            for ($i = 0, $j = $points_polygon; $i < $points_polygon; $j = $i++) {
-                if ((($vertices_y[$i]  >  $latitude_y != ($vertices_y[$j] > $latitude_y)) &&
-                    ($longitude_x < ($vertices_x[$j] - $vertices_x[$i]) * ($latitude_y - $vertices_y[$i]) / ($vertices_y[$j] - $vertices_y[$i]) + $vertices_x[$i])))
-                    $c = !$c;
-            }
-            return $c;
+        $i = $j = $c = 0;
+        for ($i = 0, $j = $pointsPolygon; $i < $pointsPolygon; $j = $i++) {
+            if ((($verticesY[$i]  >  $latitudeY != ($verticesY[$j] > $latitudeY)) &&
+                ($longitudeX < ($verticesX[$j] - $verticesX[$i]) * ($latitudeY - $verticesY[$i]) / ($verticesY[$j] - $verticesY[$i]) + $verticesX[$i])))
+                $c = !$c;
         }
+        return $c;
     }
 }
