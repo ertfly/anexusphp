e479c9b327951fb12ca38229c7bc29eadb167225
Eric Teixeira <ertfly@gmail.com>
2023-11-17 18:10:10 -0300

    fix

--- src/Core/Log.php
+++ src/Core/Log.php
@@ -8,21 +8,37 @@ class Log
 {
     public static function debug($msg)
     {
-        @file_put_contents(PATH_LOGS . 'debug_' . date('Ymd\_His') . '_' . Strings::token() . '.log', $msg);
+        if (is_array($msg)) {
+            $msg = json_encode($msg, JSON_PRETTY_PRINT);
+        }
+        $content = 'DEBUG(' . date('Y-m-d H:i:s') .  '): ' . $msg . chr(10);
+        @file_put_contents(PATH_LOGS . 'debug_' . date('Ymd') . '.log', $content, FILE_APPEND | LOCK_EX);
     }
 
     public static function error($msg)
     {
-        @file_put_contents(PATH_LOGS . 'error_' . date('Ymd\_His') . '_' . Strings::token() . '.log', $msg);
+        if (is_array($msg)) {
+            $msg = json_encode($msg, JSON_PRETTY_PRINT);
+        }
+        $content = 'ERROR(' . date('Y-m-d H:i:s') .  '): ' . $msg . chr(10);
+        @file_put_contents(PATH_LOGS . 'error_' . date('Ymd') . '.log', $content, FILE_APPEND | LOCK_EX);
     }
 
     public static function log($msg)
     {
-        @file_put_contents(PATH_LOGS . 'log_' . date('Ymd\_His') . '_' . Strings::token() . '.log', $msg);
+        if (is_array($msg)) {
+            $msg = json_encode($msg, JSON_PRETTY_PRINT);
+        }
+        $content = 'LOG(' . date('Y-m-d H:i:s') .  '): ' . $msg . chr(10);
+        @file_put_contents(PATH_LOGS . 'log_' . date('Ymd') . '.log', $content, FILE_APPEND | LOCK_EX);
     }
 
     public static function user($user, $location, $msg)
     {
-        @file_put_contents(PATH_LOGS . 'user_' . $user . '_' . $location . '_' . date('Ymd\_His') . '_' . Strings::token() . '.log', $msg);
+        if (is_array($msg)) {
+            $msg = json_encode($msg, JSON_PRETTY_PRINT);
+        }
+        $content = 'USER(' . date('Y-m-d H:i:s') .  '): ' . $msg . chr(10);
+        @file_put_contents(PATH_LOGS . 'user_' . $user . '_' . $location . '_' . date('Ymd') . '.log', $msg, FILE_APPEND | LOCK_EX);
     }
 }
